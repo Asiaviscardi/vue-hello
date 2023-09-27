4dba86d113e2c72c23dde4b3f674ae4a1939953d
Asiaviscardi <asia.viscardii@gmail.com>
2023-09-27 15:51:45 +0200

    termine esercizio

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const{createApp} = Vue;
 
 createApp({
+
     data(){
         return{
             saluto: 'Hello Vue!',
-            immagine: 'img-vue.jpg'
+            immagine: 'img-vue.jpg',
+            link: 'https://vuejs.org/',
+            testo:'Visita il sito di Vue.js!'
         }
     }
+
 }).mount('#app')
